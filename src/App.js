490da87536536aabf7e8e1d3072c40cbf3cbaecd
unknown <codingdummy@outlook.com>
2021-11-09 12:51:47 +0200

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
+export default function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <Header/>
     </div>
+    </BrowserRouter>
   );
 }
-
-export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import styles from './Header.module.css';
+import styles from './modules/Header.module.css';
 import logo from './images/logo.png';
-import about from './images/about.png';
 
 export default function Header() {
     return (
         <div className = { styles.header }>
             <div className = { styles.headerContainer }>
                 <img className = { styles.headerLogo } src = { logo } alt = "logo" />
-                <button><img className = { styles.headerAbout } src = { about } alt = "about" /></button>
-
+                <div className = { styles.headerControls }>
+                    <button className = { styles.login }>Login</button>
+                    <button className = { styles.signUp }>Sign Up</button>
+                </div>
             </div>
         </div>
     )
