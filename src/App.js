2537f5a9e0830a7f385eac2e0623cdbbd406597e
Danskutin <dante.leino@gmail.com>
2021-11-15 15:09:45 +0200

    Updated Frontend, Account Creation works now

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
-import './App.css';
+import React from 'react';
+import SignUp from './SignUpClass';
+import './components/modules/App.css';
 import Header from './components/Header';
 import Search from './components/Search';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login';
-import SignUp from './SignUp';
+import { render } from '@testing-library/react';
 
-export default function App() {
-  return (
-    <BrowserRouter>
-    <div className="App">
-      <Header/>
-      <Routes>
-        <Route path="/login" element={ <Login /> } />
-        <Route path="/signup" element={ <SignUp /> } />
-      </Routes>
-      <Search/>
-    </div>
-    </BrowserRouter>
-  );
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      items: []
+    }
+    console.log("Constructor");
+  }
+
+  addNewCustomerAccount = (email, password, firstname, lastname, address) => {
+    console.log("in addNewCustomerAccount function");
+    axios.post('http://localhost:3000/customer/addCustomer', 
+      {
+        email,
+        password,
+        firstname,
+        lastname,
+        address
+      }
+    )
+      .then(response => {
+        this.setState({ items: response.data.items })
+        console.log(JSON.stringify(response));
+        
+      })
+      .catch(err => {
+        console.log(err);
+      })
+      
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route path="/login" element={ <Login /> } />
+          <Route path="/signup" element={ <SignUp addNewCustomerAccount={ this.addNewCustomerAccount }/> } />
+        </Routes>
+        <Search/>
+      </div>
+      </BrowserRouter>
+    );
+  }
 }
+
+export default App;
---
+++ src/SignUpClass.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import styles from './components/modules/SignUpClass.module.css';
+import { Link } from 'react-router-dom'
+
+class SignUp extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          newEmail: "",
+          newPassword: "",
+          newFirstName: "",
+          newLastName: "",
+          newAddress: ""
+        }
+      }
+    
+      addNewCustomerAccount = () => {
+        this.props.addNewCustomerAccount(this.state.newEmail, this.state.newPassword, this.state.newFirstName, this.state.newLastName, this.state.newAddress);
+      }
+
+    render() {
+        return (
+            <div>
+                <div className = { styles.container }>
+                    <div className = { styles.formContainer }> 
+                    <form className = { styles.email }>
+                        <label>
+                        Email:
+                        </label>
+                        <input type="text" id="email" onChange={ (event) => this.setState({ newEmail: event.target.value }) }/>
+                        <label>
+                        Password:
+                        </label>
+                        <input type="password" id="password" onChange={ (event) => this.setState({ newPassword: event.target.value }) }/>
+                        <label>
+                        {/* Repeat Password:
+                        </label>
+                        <input type="password" id="password" onChange={ (event) => this.setState({ newGameName: event.target.value }) }/>
+                        <label> */}
+                        First Name:
+                        </label>
+                        <input type="text" id="name" onChange={ (event) => this.setState({ newFirstName: event.target.value }) }/>
+                        <label>
+                        Last Name:
+                        </label>
+                        <input type="text" id="name" onChange={ (event) => this.setState({ newLastName: event.target.value }) }/>
+                        <label>
+                        Address
+                        </label>
+                        <input type="text" id="address" onChange={ (event) => this.setState({ newAddress: event.target.value }) }/>
+                    </form>
+                    </div>
+                    <div className = { styles.buttonContainer }>
+                        <button onClick={ this.addNewCustomerAccount } className = { styles.signUpButton}> Create Account
+                        </button>
+                        <Link to ="/login"><button className = { styles.signUpButton}>Back To Login</button></Link>
+                    </div>
+                </div>
+            </div>
+        
+        )
+    }
+}
+
+export default SignUp;
