a0e47de25011b90a033516417abc21dc80f9f0f7
unknown <codingdummy@outlook.com>
2021-11-15 13:43:04 +0200

    WIP

---
+++ src/components/data/restaurant.js
@@ -0,0 +1,56 @@
+export const restaurants = [
+    {
+        "restaurantID":       1,
+        "name":               "Hesburger",
+        "address":            "Test Address",
+        "type":               "Fast food",
+        "price":              "€",
+        "hours":              "Mon-Fri: 10:00-22:00",
+        "image":              require("../images/chestfull.png")
+    },
+    {
+        "restaurantID":       2,
+        "name":               "Hesburger",
+        "address":            "Test Address",
+        "type":               "Fast food",
+        "price":              "€",
+        "hours":              "Mon-Fri: 10:00-22:00",
+        "image":              "image path"
+    },
+    {
+        "restaurantID":       3,
+        "name":               "Hesburger",
+        "address":            "Test Address",
+        "type":               "Fast food",
+        "price":              "€",
+        "hours":              "Mon-Fri: 10:00-22:00",
+        "image":              "image path"
+    },
+    {
+        "restaurantID":       4,
+        "name":               "Hesburger",
+        "address":            "Test Address",
+        "type":               "Fast food",
+        "price":              "€",
+        "hours":              "Mon-Fri: 10:00-22:00",
+        "image":              "image path"
+    },
+    {
+        "restaurantID":       5,
+        "name":               "Hesburger",
+        "address":            "Test Address",
+        "type":               "Fast food",
+        "price":              "€",
+        "hours":              "Mon-Fri: 10:00-22:00",
+        "image":              "image path"
+    },
+    {
+        "restaurantID":       6,
+        "name":               "Hesburger",
+        "address":            "Test Address",
+        "type":               "Fast food",
+        "price":              "€",
+        "hours":              "Mon-Fri: 10:00-22:00",
+        "image":              "image path"
+    }
+]
