23259ab91f8ae70113c4a8f72fbb6f20926f2d8a
unknown <codingdummy@outlook.com>
2021-11-23 12:11:18 +0200

    Updated signup and manager signup

--- src/AddRestaurantClass.js
+++ src/AddRestaurantClass.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './components/modules/AddRestaurantClass.module.css';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 class AddRestaurant extends React.Component {
     
@@ -25,46 +25,32 @@ class AddRestaurant extends React.Component {
             <div>
                 <div className = { styles.container }>
                     <div className = { styles.formContainer }> 
-                    <form className = { styles.email }>
-                        <label>
-                        Restaurant Name:
-                        </label>
-                        <input type="text" id="restaurantName" onChange={ (event) => this.setState({ newRestaurantName: event.target.value }) } value={this.state.newRestaurantName}/>
-                        <label>
-                        Address:
-                        </label>
-                        <input type="text" id="restaurantAddress" onChange={ (event) => this.setState({ newAddress: event.target.value }) } value={this.state.newAddress}/>
-                        <label>
-                         Restaurant Type:
-                        </label>
-                        <input type="text" id="restaurantType" onChange={ (event) => this.setState({ newRestaurantType: event.target.value }) } value={this.state.newRestaurantType}/>
-                        <label>
-                        Price Level
-                        </label>
-                        <div className= { styles.priceLevel } onChange={ (event) => this.setState({newPriceLevel: event.target.value}) } value={this.state.newPriceLevel}>
-                            <input type="radio" name="priceLevel" value = "€" /> €   
-                            <input type="radio" name="priceLevel" value = "€€" /> €€
-                            <input type="radio" name="priceLevel" value = "€€€" /> €€€
-                            <input type="radio" name="priceLevel" value = "€€€€" /> €€€€
-                        </div>
-                        <label>
-                        Operating Hours:
-                        </label>
-                        <input type="text" id="restaurantOperatingHours" onChange={ (event) => this.setState({ newOperatingHours: event.target.value }) } value={this.state.newOperatingHours}/>
-                        <label>
-                        Restaurant Description:
-                        </label>
-                        <input type="text" id="restaurantDescription" onChange={ (event) => this.setState({ newRestaurantDescription: event.target.value }) } value={this.state.newRestaurantDescription}/>
-                        <div>
-                            Add restaurant photo <input type= "file" name="photo" />
-                        </div>
-                    </form>
+                        <form className = { styles.email }>
+                            <label>Restaurant Name:</label>
+                            <input type="text" id="restaurantName" onChange={ (event) => this.setState({ newRestaurantName: event.target.value }) } value={this.state.newRestaurantName}/>
+                            <label>Address:</label>
+                            <input type="text" id="restaurantAddress" onChange={ (event) => this.setState({ newAddress: event.target.value }) } value={this.state.newAddress}/>
+                            <label>Restaurant Type:</label>
+                            <input type="text" id="restaurantType" onChange={ (event) => this.setState({ newRestaurantType: event.target.value }) } value={this.state.newRestaurantType}/>
+                            <label>Price Level</label>
+                            <div className= { styles.priceLevel } onChange={ (event) => this.setState({newPriceLevel: event.target.value}) } value={this.state.newPriceLevel}>
+                                <input type="radio" name="priceLevel" value = "€" /> €   
+                                <input type="radio" name="priceLevel" value = "€€" /> €€
+                                <input type="radio" name="priceLevel" value = "€€€" /> €€€
+                                <input type="radio" name="priceLevel" value = "€€€€" /> €€€€
+                            </div>
+                            <label>Operating Hours:</label>
+                            <input type="text" id="restaurantOperatingHours" onChange={ (event) => this.setState({ newOperatingHours: event.target.value }) } value={this.state.newOperatingHours}/>
+                            <label>Restaurant Description:</label>
+                            <input type="text" id="restaurantDescription" onChange={ (event) => this.setState({ newRestaurantDescription: event.target.value }) } value={this.state.newRestaurantDescription}/>
+                            <div>
+                                Add restaurant photo <input type= "file" name="photo" />
+                            </div>
+                        </form>
                     </div>
                     <div className = { styles.buttonContainer }>
                         <button id="addNewRestaurantButton" onClick={ this.addNewRestaurant } disabled={!this.state.newRestaurantName | !this.state.newAddress | !this.state.newRestaurantType | !this.state.newAddress | !this.state.newOperatingHours | !this.state.newRestaurantDescription}
-                        className = { styles.signUpButton }> Create Restaurant
-                        </button>
-                        <Link to ="/login"><button className = { styles.signUpButton }>Back To Login</button></Link>
+                        className = { styles.signUpButton }>Create Restaurant</button>
                     </div>
                 </div>
             </div>
--- src/ManagerSignUpView.js
+++ src/ManagerSignUpView.js
@@ -15,24 +15,24 @@ export default function SignUpView(props) {
     return (
         <div>
             <div>
-          <h1>Add New Manager</h1>
-          <div>
-            Email <input type="email" placeholder="your@email.com" onChange={ (event) => addNewEmail(event.target.value) } />
-          </div>
-          <div>
-            Password <input type="password" onChange={ (event) => addNewPassword(event.target.value) } />
-          </div>
-          <div>
-            First name <input type="text" onChange={ (event) => addNewFirstName(event.target.value) } />
-          </div>
-          <div>
-            Last name <input type="text" onChange={ (event) => addNewLastName(event.target.value) } />
-          </div>
-          <div>
-            Manager Authentication <input type="text" onChange={ (event) => addNewManagerAuthentication(event.target.value) } />
-          </div>
-          <button onClick={ addNewManagerAccount }>Create account</button>
-          </div>
+                <h1>Add New Manager</h1>
+                <div>
+                    Email<input type="email" placeholder="your@email.com" onChange={ (event) => addNewEmail(event.target.value) } />
+                </div>
+                <div>
+                    Password <input type="password" onChange={ (event) => addNewPassword(event.target.value) } />
+                </div>
+                <div>
+                    First name <input type="text" onChange={ (event) => addNewFirstName(event.target.value) } />
+                </div>
+                <div>
+                    Last name <input type="text" onChange={ (event) => addNewLastName(event.target.value) } />
+                </div>
+                <div>
+                    Manager Authentication <input type="text" onChange={ (event) => addNewManagerAuthentication(event.target.value) } />
+                </div>
+                <button onClick={ addNewManagerAccount }>Create account</button>
+            </div>
         </div>
     )
 }
