bb61ecea868630157ff3cd51c7dda1636cfb2a66
unknown <codingdummy@outlook.com>
2021-11-24 15:00:19 +0200

    Updated addRestaurant page

--- src/AddRestaurantClass.js
+++ src/AddRestaurantClass.js
@@ -44,34 +44,37 @@ class AddRestaurant extends React.Component {
             <div>
                 <div className = { styles.container }>
                     <div className = { styles.formContainer }> 
-                        <form className = { styles.email }>
-                            <label>Restaurant Name:</label>
-                            <input type="text" id="restaurantName" onChange={ (event) => this.setState({ newRestaurantName: event.target.value }) } value={this.state.newRestaurantName}/>
-                            <label>Address:</label>
-                            <input type="text" id="restaurantAddress" onChange={ (event) => this.setState({ newAddress: event.target.value }) } value={this.state.newAddress}/>
-                            <label>Restaurant Type:</label>
-                            <input type="text" id="restaurantType" onChange={ (event) => this.setState({ newRestaurantType: event.target.value }) } value={this.state.newRestaurantType}/>
-                            <label>Price Level</label>
-                            <div className= { styles.priceLevel } onChange={ (event) => this.setState({newPriceLevel: event.target.value}) } value={this.state.newPriceLevel}>
-                                <input type="radio" name="priceLevel" value = "€" /> €   
-                                <input type="radio" name="priceLevel" value = "€€" /> €€
-                                <input type="radio" name="priceLevel" value = "€€€" /> €€€
-                                <input type="radio" name="priceLevel" value = "€€€€" /> €€€€
+                        <div className = { styles.restaurantFields }>
+                            <div className = { styles.form }>
+                                <label>Restaurant Name:</label>
+                                <input type="text" id="restaurantName" onChange={ (event) => this.setState({ newRestaurantName: event.target.value }) } value={this.state.newRestaurantName}/>
+                                <label>Address:</label>
+                                <input type="text" id="restaurantAddress" onChange={ (event) => this.setState({ newAddress: event.target.value }) } value={this.state.newAddress}/>
+                                <label>Restaurant Type:</label>
+                                <input type="text" id="restaurantType" onChange={ (event) => this.setState({ newRestaurantType: event.target.value }) } value={this.state.newRestaurantType}/>
                             </div>
-                            <label>Operating Hours:</label>
-                            <input type="text" id="restaurantOperatingHours" onChange={ (event) => this.setState({ newOperatingHours: event.target.value }) } value={this.state.newOperatingHours}/>
-                            <label>Restaurant Description:</label>
-                            <input type="text" id="restaurantDescription" onChange={ (event) => this.setState({ newRestaurantDescription: event.target.value }) } value={this.state.newRestaurantDescription}/>
-                            <div>
-                            Add restaurant photo <input type= "file" onChange={this.onImageChange} className="filetype" id="group_image" />
-                            <img id="target" src={this.state.image} width="200" height="100"/>
+                            <div className = { styles.form }>
+                                <label>Operating Hours:</label>
+                                <input type="text" id="restaurantOperatingHours" onChange={ (event) => this.setState({ newOperatingHours: event.target.value }) } value={this.state.newOperatingHours}/>
+                                <label>Restaurant Description:</label>
+                                <input type="text" id="restaurantDescription" onChange={ (event) => this.setState({ newRestaurantDescription: event.target.value }) } value={this.state.newRestaurantDescription}/>
+                                <label>Price Level:</label>
+                                <div className= { styles.priceLevel } onChange={ (event) => this.setState({newPriceLevel: event.target.value}) } value={this.state.newPriceLevel}>
+                                    <input type="radio" name="priceLevel" value = "€" /> €   
+                                    <input type="radio" name="priceLevel" value = "€€" /> €€
+                                    <input type="radio" name="priceLevel" value = "€€€" /> €€€
+                                    <input type="radio" name="priceLevel" value = "€€€€" /> €€€€
+                                </div>
+                            </div>
                         </div>
-                        <p color="red">{this.state.fileName}</p>
-                        </form>
+                    </div>
+                    <div className = { styles.restaurantImage }>
+                        <p>Add restaurant photo</p> 
+                        <input type= "file" onChange={this.onImageChange} className= { styles.filetype } id="group_image" />
+                        <img className = { styles.imageTarget } id="target" src={this.state.image} alt=""/>
                     </div>
                     <div className = { styles.buttonContainer }>
-                        <button id="addNewRestaurantButton" onClick={ this.addNewRestaurant } disabled={!this.state.newRestaurantName | !this.state.newAddress | !this.state.newRestaurantType | !this.state.newAddress | !this.state.newOperatingHours | !this.state.newRestaurantDescription}
-                        className = { styles.signUpButton }>Create Restaurant</button>
+                        <button id="addNewRestaurantButton" onClick={ this.addNewRestaurant } disabled={!this.state.newRestaurantName | !this.state.newAddress | !this.state.newRestaurantType | !this.state.newAddress | !this.state.newOperatingHours | !this.state.newRestaurantDescription}>Create Restaurant</button>
                     </div>
                 </div>
             </div>
