01b9aaa45e726f2c27a4d0bf21e227225e61cd04
Danskutin <dante.leino@gmail.com>
2021-11-24 10:31:22 +0200

    Small changes to addRestaurant

--- src/AddRestaurantClass.js
+++ src/AddRestaurantClass.js
@@ -44,44 +44,6 @@ class AddRestaurant extends React.Component {
             <div>
                 <div className = { styles.container }>
                     <div className = { styles.formContainer }> 
-<<<<<<< HEAD
-                    <form className = { styles.email }>
-                        <label>
-                        Restaurant Name:
-                        </label>
-                        <input type="text" id="restaurantName" onChange={ (event) => this.setState({ newRestaurantName: event.target.value }) } value={this.state.newRestaurantName}/>
-                        <label>
-                        Address:
-                        </label>
-                        <input type="text" id="restaurantAddress" onChange={ (event) => this.setState({ newAddress: event.target.value }) } value={this.state.newAddress}/>
-                        <label>
-                         Restaurant Type:
-                        </label>
-                        <input type="text" id="restaurantType" onChange={ (event) => this.setState({ newRestaurantType: event.target.value }) } value={this.state.newRestaurantType}/>
-                        <label>
-                        Price Level
-                        </label>
-                        <div className= { styles.priceLevel } onChange={ (event) => this.setState({newPriceLevel: event.target.value}) } value={this.state.newPriceLevel}>
-                            <input type="radio" name="priceLevel" value = "€" /> €   
-                            <input type="radio" name="priceLevel" value = "€€" /> €€
-                            <input type="radio" name="priceLevel" value = "€€€" /> €€€
-                            <input type="radio" name="priceLevel" value = "€€€€" /> €€€€
-                        </div>
-                        <label>
-                        Operating Hours:
-                        </label>
-                        <input type="text" id="restaurantOperatingHours" onChange={ (event) => this.setState({ newOperatingHours: event.target.value }) } value={this.state.newOperatingHours}/>
-                        <label>
-                        Restaurant Description:
-                        </label>
-                        <input type="text" id="restaurantDescription" onChange={ (event) => this.setState({ newRestaurantDescription: event.target.value }) } value={this.state.newRestaurantDescription}/>
-                        <div>
-                            Add restaurant photo <input type= "file" onChange={this.onImageChange} className="filetype" id="group_image" />
-                            <img id="target" src={this.state.image} width="200" height="100"/>
-                        </div>
-                        <p color="red">{this.state.fileName}</p>
-                    </form>
-=======
                         <form className = { styles.email }>
                             <label>Restaurant Name:</label>
                             <input type="text" id="restaurantName" onChange={ (event) => this.setState({ newRestaurantName: event.target.value }) } value={this.state.newRestaurantName}/>
@@ -101,10 +63,11 @@ class AddRestaurant extends React.Component {
                             <label>Restaurant Description:</label>
                             <input type="text" id="restaurantDescription" onChange={ (event) => this.setState({ newRestaurantDescription: event.target.value }) } value={this.state.newRestaurantDescription}/>
                             <div>
-                                Add restaurant photo <input type= "file" name="photo" />
-                            </div>
+                            Add restaurant photo <input type= "file" onChange={this.onImageChange} className="filetype" id="group_image" />
+                            <img id="target" src={this.state.image} width="200" height="100"/>
+                        </div>
+                        <p color="red">{this.state.fileName}</p>
                         </form>
->>>>>>> 23259ab91f8ae70113c4a8f72fbb6f20926f2d8a
                     </div>
                     <div className = { styles.buttonContainer }>
                         <button id="addNewRestaurantButton" onClick={ this.addNewRestaurant } disabled={!this.state.newRestaurantName | !this.state.newAddress | !this.state.newRestaurantType | !this.state.newAddress | !this.state.newOperatingHours | !this.state.newRestaurantDescription}
