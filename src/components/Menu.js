0d1776191c387baff2226b6c567aa6241e4ae7d8
Tuomas Riihiaho <78533799+c0ritu00@users.noreply.github.com>
2021-12-12 16:59:24 +0200

    test

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -23,14 +23,8 @@ class AddProduct extends React.Component {
           cartItems: [],
           product: "",
           rememberMe: false,
-          redirectAddress: "",
-          amountOfItem: 0,
 
-          idShoppingCart: "1", // TUOMAS WORK THIS OUT PLEASE
-          cartItemId: "",
-          cartItemName: "",
-          cartItemPrice: "",
-          cartItemAmount: 0
+          redirectAddress: ""
         }
       }
       
@@ -38,95 +32,66 @@ class AddProduct extends React.Component {
     componentDidMount() {
         axios.get(Constants.API_ADDRESS + '/product')
             .then(response => {
-                // console.log(response);
+                console.log(response);
                 this.setState({ products: response.data })
             })
             .catch(error => {
                 console.log(error);
                 this.setState({errorMsg: 'Error retrieving data'})
             })
-        
-        // axios.get(Constants.API_ADDRESS + '/shoppingcart')
-        //     .then(response => {
-        //       // console.log(response);
-        //       this.setState({ cartItems: response.data })
-        // })
-        // .catch(error => {
-        //     console.log(error);
-        //     this.setState({errorMsg: 'Error retrieving data'})
-        // })
+            const rememberMe = localStorage.getItem('rememberMe') === 'true';
+            const product = rememberMe ? localStorage.getItem('product') : '';
+            this.setState({ product, rememberMe });
     }
 
-    manageMenu = (id) => {
-      const menuEditAddress = ("/menuedit/" + id);
-      // this.setState({redirectAddress: menuEditAddress});
-      this.props.manageMenu(menuEditAddress);
-    }
+      manageMenu = (id) => {
+        this.props.manageMenu(id);
+        const menuEditAddress = ("/menuedit/" + id);
+        this.setState({redirectAddress: menuEditAddress});
+      }
 
-    // onAddItem = (addedItem) => {
-    //   this.setState(prev => ({cartItems: [...prev.cartItems, addedItem], addedItem: ''}))
+      onAdd = (product) => {
+        // const exist = this.state.cartItems.find((x) => x.id === product.id);
+        const exist = false;
+        console.log(this.state.cartItems);
+        console.log(exist);
+        console.log(product);
 
-    //   this.setState({amountOfItem: this.state.amountOfItem+1});
+        if (exist) {
+          this.cartItems.setState(
+            this.state.cartItems.map((x) =>
+              x.id === product.id ? { ...this.state.cartItems, qty: product.qty + 1 } : x
+            )
+          );
+        } else {
+        //   this.cartItems.setState([...this.state.cartItems, { ...product, qty: 1 }]);this.state.cartItems
+        }
+      };
 
-    //   console.log("This is the value of the item: ");
-    //   console.log(this.state.amountOfItem);
+      onSubmit = () => {
+        const { product, rememberMe } = this.state;
+        localStorage.setItem('rememberMe', rememberMe);
+        localStorage.setItem('product', rememberMe ? product : product.productname);
+        console.log('this is product' +product);
+      }
 
-    //   console.log(this.state.cartItems);
-    // }
+      
 
-    // onRemoveItem = (removedItem) => {
-    //   const itemsInCart = this.state.cartItems.filter(item => item.productid !== removedItem.productid);
-    //   this.setState({cartItems: itemsInCart});
+      // <SearchView
+      //           restaurants={ this.filteredItems() }
 
-    //   console.log('Item removed from shopping cart: ')
-    //   console.log(removedItem.productname + " " + removedItem.productprice)
-    // }
-
-    onAddItemToCart = (addedItem) => {
-      const idshoppingcart = this.state.idShoppingCart;
-      const cartitemid = addedItem.productid;
-      const cartitemname = addedItem.productname;
-      const cartitemprice = addedItem.productprice;
-      const cartitemamount = this.state.amountOfItem+1;
-
-      this.setState({idShoppingCart: idshoppingcart})
-      this.setState({cartItemId: cartitemid})
-      this.setState({cartItemName: cartitemname})
-      this.setState({cartItemPrice: cartitemprice})
-      this.setState({cartItemAmount: cartitemamount})
-
-      // console.log(this.state.idShoppingCart)
-      // console.log(this.state.cartItemId)
-      // console.log(this.state.cartItemName)
-      // console.log(this.state.cartItemPrice)
-      // console.log(this.state.cartItemAmount)
-
-      this.props.onAddItemToCart(this.state.idShoppingCart, this.state.cartItemId, this.state.cartItemName, this.state.cartItemPrice, this.state.cartItemAmount);
-      // window.location.reload();
-
-      console.log(this.state.cartItems);
-    }
-
-    onRemoveItemFromCart = (chosenItem) => {
-      const cartitemid = chosenItem.productid;
-      this.setState({cartItemId: cartitemid})
-      this.props.onRemoveItemFromCart(this.state.cartItemId);
-      // window.location.reload();
-    }
-
-    updatedPage = () => {
-      console.log(this.state.cartItems);
-    }
+      //       />
+    
 
     render() {
 
-        const { products, errorMsg, cartItems } = this.state
+        const { products, errorMsg } = this.state
         const restaurantid = window.location.href.slice(-2)
         const filteredProducts = () => {
             const productsArray = products.filter((product) => product.restaurantpageid.toString().toLowerCase().includes(restaurantid.toLowerCase()));
             return productsArray
           }
-          // console.log(products);
+          console.log(products);
         const filteredProductsArray = filteredProducts();
 
         return (
@@ -135,7 +100,7 @@ class AddProduct extends React.Component {
                     {filteredProductsArray.length ?
                         filteredProductsArray.map(filteredproduct => 
                         <div className={styles.product} key={filteredproduct.id}>
-                            <div className={styles.productImageContainer}>
+                            <div className={styles.productImage}>
                                 <img className={styles.productImage} src={filteredproduct.productimage} alt="" />
                             </div>
                             <div className={styles.productName}>
@@ -146,26 +111,8 @@ class AddProduct extends React.Component {
                                 <p>{filteredproduct.productprice} €</p>
                             </div>
                             <div className={styles.productButtons}>
-                                <button id="addToShoppingCart" key={filteredProductsArray.productid} onClick={() => this.onAddItemToCart(filteredproduct) } > +</button>
-                                <button id="removeFromShoppingCart" key={filteredProductsArray.productid} onClick={() => this.onRemoveItemFromCart(filteredproduct) }> - </button>
-                            </div>
-                        </div>) :
-                        null}
-                    {errorMsg ? <div>{errorMsg}</div> : null}
-                </div>
-                <div className={styles.productContainer}>
-                    {cartItems.length ?
-                        cartItems.map(cartItem => 
-                        <div className={styles.product} key={cartItem.idcartitem}>
-                            <div className={styles.productImage}>
-                                <img className={styles.productImage} src={cartItem.productimage} alt="" />
-                            </div>
-                            <div className={styles.productName}>
-                                <h1 >{cartItem.productname}</h1>
-                                <h2>{cartItem.productdescription}</h2>
-                            </div>
-                            <div className={styles.productPrice}>
-                                <p>{cartItem.productprice} €</p>
+                                <button id="addItem" onClick={/*this.onAdd(product),*/ this.onSubmit} > +</button>
+                                <button id=""> - </button>
                             </div>
                         </div>) :
                         null}
