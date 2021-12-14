0e356252e0efaaacfaaa528efdf57aa9d3c1a6ee
Tuomas Riihiaho <78533799+c0ritu00@users.noreply.github.com>
2021-12-14 16:01:40 +0200

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,20 +10,38 @@ export default function Header(props) {
 
 
     let frontPageRoute = <>
-    
         <Link to="/"><img className = { styles.headerLogo } src = { logo } alt = "logo" /></Link>
     </>
 
+    let loginButton = <>
+        <Link to="login"><button className = { styles.login }>Login</button></Link>
+    </>
 
-    if(props.isUserLoggedIn == true) {
+    let logOutButton = <>
+        <Link to="/"><button  className = { styles.login } onClick={ props.logout } hidden>Logout</button></Link>
+    </>
+
+    let shoppingCart = <>
+        <Link to="/shoppingcart"> <img className = { styles.cart } src = { cart } alt = "cart" hidden/> </Link>
+    </>
+
+    if(props.isUserLoggedIn === true) {
         frontPageRoute = <Link to="/ProtectedCustomer"><img className = { styles.headerLogo } src = { logo } alt = "logo" /></Link>
     }
-    if (props.isManagerLoggedIn == true) {
+    if (props.isManagerLoggedIn === true) {
         frontPageRoute = <Link to="/ProtectedManager"><img className = { styles.headerLogo } src = { logo } alt = "logo" /></Link>
     }
-    // if (props.isManagerLoggedIn == true) {
-    //     frontPageRoute = <Link to="/ProtectedManager"><img className = { styles.headerLogo } src = { logo } alt = "logo" /></Link>
-    // }
+    if (props.isManagerLoggedIn || props.isUserLoggedIn === true) {
+        loginButton = <Link to="login"><button className = { styles.login } hidden>Login</button></Link>
+    }
+    if (props.isManagerLoggedIn || props.isUserLoggedIn === true) {
+        logOutButton = <Link to="/"><button  className = { styles.login } onClick={ props.logout }>Logout</button></Link>
+    }
+  
+    if (props.isManagerLoggedIn || props.isUserLoggedIn === true) {
+        shoppingCart = <Link to="/shoppingcart"> <img className = { styles.cart } src = { cart } alt = "cart" /> </Link>
+    }
+
 
     return (
         
@@ -31,12 +49,11 @@ export default function Header(props) {
             <div className = { styles.headerContainer }>
             { frontPageRoute }
                 <div className = { styles.headerControls }>
-                    
-                    
-                    <Link to="login"><button className = { styles.login }>Login</button></Link>
-                    <Link to="shoppingcart"> <img className = { styles.cart } src = { cart } alt = "cart" /> </Link>
-                    <Link to="/"><button  className = { styles.login } onClick={ props.logout }>Logout</button></Link>
-                    
+                      
+                    { loginButton }
+                    { logOutButton }
+                    { shoppingCart }     
+                               
                 </div>
             </div>
         </div>
